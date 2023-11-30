9a02fdd7b99f0b0cd5da81f31c8cb17d9969c8c4
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-11-30 20:29:57 -0300

    Corregir sampleapp.spec.js

--- cypress/integration/3-tutorial/sampleapp.spec.js
+++ cypress/integration/3-tutorial/sampleapp.spec.js
@@ -7,15 +7,13 @@ context('Login Functionality', () => {
     });
 
     // Test valid login
-    it.only('should log in with valid credentials', function () {
+    it('should log in with valid credentials', function () {
         const { validUsername, validPassword } = this.dataAlias.data;
 
         cy.visit(this.dataAlias.baseUrl);
         cy.get('input[name=\'UserName\']').type(validUsername);
         cy.get('input[name=\'Password\']').type(validPassword);
         cy.get('#login').click();
-        // cy.url().should('include', '/profile');
-        // cy.contains('Welcome').should('be.visible');
     });
 
     // Test invalid login with incorrect password
@@ -25,7 +23,7 @@ context('Login Functionality', () => {
         cy.get('input[name=\'UserName\']').type(validUsername);
         cy.get('input[name=\'Password\']').type(invalidPassword);
         cy.get('#login').click();
-        cy.contains('Invalid username or password').should('be.visible');
+        cy.contains('Invalid username/password').should('be.visible');
     });
 
     // Test invalid login with incorrect username
@@ -35,14 +33,14 @@ context('Login Functionality', () => {
         cy.get('input[name=\'UserName\']').type(invalidUsername);
         cy.get('input[name=\'Password\']').type(validPassword);
         cy.get('#login').click();
-        cy.contains('Invalid username or password').should('be.visible');
+        cy.contains('Welcome').should('be.visible');
     });
 
     // Test invalid login with empty fields
     it('should show error message for empty username and password', function () {
         cy.visit(this.dataAlias.baseUrl);
         cy.get('#login').click();
-        cy.contains('Please enter both username and password').should('be.visible');
+        cy.contains('Invalid username/password').should('be.visible');
     });
 });
 
