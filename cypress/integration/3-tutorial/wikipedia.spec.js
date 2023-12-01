a926cb99f720ed73d836cd67889caf60e4a1a198
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-11-30 21:33:14 -0300

    Agregar BBD expectations e Implicit waits

--- cypress/integration/3-tutorial/wikipedia.spec.js
+++ cypress/integration/3-tutorial/wikipedia.spec.js
@@ -4,8 +4,29 @@ context('Wikipedia Test', () => {
         cy.fixture('wikipedia').as('wiki');
     });
 
-    it('Test 01 - BDD extracitions', function() {
+    it('Test 01 - BDD expectations', function() {
        cy.visit(this.wiki.baseUrl);
-       cy.get('#www-wikipedia-org')
+       cy.get('#www-wikipedia-org #searchInput').type("Globant {enter}");
+
+       cy.get('.infobox img').eq(0).should('be.visible');
+
+       cy.get('.mediawiki  #firstHeading').should('be.visible');
+
+        cy.get('.mediawiki  #firstHeading').should('contain', "Globant");
+
+    });
+
+    it('Test 02 - Implicit Wait', function() {
+        cy.visit(this.wiki.baseUrl);
+        cy.get('#www-wikipedia-org #searchInput').type("Globant {enter}");
+
+        cy.wait(6000);
+
+        cy.get('.infobox img').eq(0).should('be.visible');
+
+        cy.get('.mediawiki  #firstHeading').should('be.visible');
+
+        cy.get('.mediawiki  #firstHeading').should('contain', "Globant");
+
     });
 });
