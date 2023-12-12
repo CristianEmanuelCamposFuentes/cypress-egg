f1e5bfe52e38a055fa804d7f24bdab9cd80904a9
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-12-11 23:00:04 -0300

    Crear qavalidation.spec.js

---
+++ cypress/integration/3-tutorial/qavalidation.spec.js
@@ -0,0 +1,48 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Prevent Cypress from failing the test on uncaught exceptions
+    return false;
+});
+
+context('Login Functionality', () => {
+    // Load test data from a fixture file
+    beforeEach(() => {
+        cy.fixture('credentials').as('dataAlias');
+    });
+
+    // Test valid login
+    it('should log in with valid credentials', function () {
+        const { validUsername, validPassword } = this.dataAlias.data;
+
+        cy.visit(this.dataAlias.baseUrl);
+        cy.get('input[name=\'UserName\']').type(validUsername);
+        cy.get('input[name=\'Password\']').type(validPassword);
+        cy.get('#login').click();
+    });
+
+    // Test invalid login with incorrect password
+    it('should show error message for invalid password', function () {
+        const { validUsername, invalidPassword } = this.dataAlias.data;
+        cy.visit(this.dataAlias.baseUrl);
+        cy.get('input[name=\'UserName\']').type(validUsername);
+        cy.get('input[name=\'Password\']').type(invalidPassword);
+        cy.get('#login').click();
+        cy.contains('Invalid username/password').should('be.visible');
+    });
+
+    // Test invalid login with incorrect username
+    it('should show error message for invalid username', function () {
+        const { invalidUsername, validPassword } = this.dataAlias.data;
+        cy.visit(this.dataAlias.baseUrl);
+        cy.get('input[name=\'UserName\']').type(invalidUsername);
+        cy.get('input[name=\'Password\']').type(validPassword);
+        cy.get('#login').click();
+        cy.contains('Welcome').should('be.visible');
+    });
+
+    // Test invalid login with empty fields
+    it('should show error message for empty username and password', function () {
+        cy.visit(this.dataAlias.baseUrl);
+        cy.get('#login').click();
+        cy.contains('Invalid username/password').should('be.visible');
+    });
+});
